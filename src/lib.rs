75b298b14346f80a1917ec31ed9f0f5a9bddb141
0xAA55 <weixiaobao213@qq.com>
2025-08-10 15:23:54 +0800

    修改单元测试，使调试通过，并进行定时 10 秒的持续渲染测试。

--- src/lib.rs
+++ src/lib.rs
@@ -18,31 +18,29 @@ mod tests {
         window.make_current();
         glfw.set_swap_interval(SwapInterval::Adaptive);
 
-        let glcore = GLCore::new(|proc_name|window.get_proc_address(proc_name));
-
+        let glcore = GLCore::new(|proc_name|window.get_proc_address(proc_name)).unwrap();
         dbg!(glcore);
 
+        let start_time = glfw.get_time();
         while !window.should_close() {
-            glfw.poll_events();
-            for (_, event) in glfw::flush_messages(&events) {
-                handle_window_event(&mut window, event);
-            }
             let cur_frame_time = glfw.get_time();
 
-            glcore.glClearColor(cur_frame_time.sin() as f32, cur_frame_time.cos() as f32, (cur_frame_time * 1.5).sin() as f32, 1.0);
-            glcore.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glcore.glClearColor(cur_frame_time.sin() as f32, cur_frame_time.cos() as f32, (cur_frame_time * 1.5).sin() as f32, 1.0).unwrap();
+            glcore.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT).unwrap();
 
             window.swap_buffers();
-        }
-    }
-
-    #[allow(dead_code)]
-    fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-        match event {
-            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            glfw.poll_events();
+            for (_, event) in glfw::flush_messages(&events) {
+                match event {
+                    glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                        window.set_should_close(true)
+                    }
+                    _ => {}
+                }
+            }
+            if cur_frame_time - start_time >= 10.0 {
                 window.set_should_close(true)
             }
-            _ => {}
         }
     }
 }
